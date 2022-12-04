406ed5d16861d313e207e4f7bd9c47df3e0e7f87
OlegPustovalov <106656451+OlegPustovalov@users.noreply.github.com>
2022-12-04 12:19:02 +0300

    Add insert  school_student_teachers table SQL

---
+++ 2.2-databases-2/orm_migrations/insert school_student_teachers.sql
@@ -0,0 +1,12 @@
+insert into school_student_teachers 
+values (1,1,1);
+insert into school_student_teachers 
+values (2,1,2);
+insert into school_student_teachers 
+values (3,2,1);
+insert into school_student_teachers 
+values (4,2,3);
+insert into school_student_teachers 
+values (5,3,2);
+insert into school_student_teachers 
+values (6,2,2);
