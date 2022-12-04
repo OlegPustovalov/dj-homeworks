647a9a782db7b06d94d0dfae5b5b574ad4ed19e1
OlegPustovalov <106656451+OlegPustovalov@users.noreply.github.com>
2022-12-05 00:40:49 +0300

    Add insert tag scope SQL tables ver 041222

---
+++ 2.2-databases-2/m2m-relations/insert tag scope tables SQL.sql
@@ -0,0 +1,16 @@
+insert into articles_tag  
+values (1,'космос');
+insert into articles_tag 
+values (2,'культура');
+insert into articles_tag 
+values (3,'наука');
+insert into articles_tag 
+values (4,'город');
+insert into articles_scope  
+values (1,1,4);
+insert into articles_scope  
+values (2,1,3);
+insert into articles_scope  
+values (3,3,1);
+insert into articles_scope 
+values (4,3,2);
