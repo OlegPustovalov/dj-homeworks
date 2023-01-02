d5cdcbd043c463e1b9b3051957671e94f9d5c667
OlegPustovalov <106656451+OlegPustovalov@users.noreply.github.com>
2023-01-02 14:10:13 +0300

    models serializers views urls ver 020123

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,21 @@
+from django.contrib import admin
 from django.urls import path
+
+from rest_framework.routers import DefaultRouter
+from measurement.views import SensorViewSet, MeasurementViewSet, MeasurementView, SensorView,SensorwithmeasurementsView
+
+r1 = DefaultRouter()
+r1.register ('sensor',SensorViewSet)
+
+r2 = DefaultRouter()
+r2.register ('measurement', MeasurementViewSet)
+
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
-]
+    path('admin/', admin.site.urls),
+    path('measure/<pk>/', MeasurementView.as_view()),
+    path('sens/<pk>/', SensorView.as_view()),
+    path('sensors/<pk>/', SensorwithmeasurementsView.as_view()),
+]+ r1.urls+r2.urls
+
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,38 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from django.shortcuts import render
+from rest_framework.response import Response
+
+from rest_framework.generics import RetrieveAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+# создание датчика и получение списка датчиков
+class SensorViewSet(ModelViewSet):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+# добавление измерения и получения списка всех измерений
+class MeasurementViewSet(ModelViewSet):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+    
+# вывод детальной информации по измерениям по конкретному датчику, в URL добавляется его primary key
+class SensorwithmeasurementsView(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+# обновление и удаление конкретного датчика, вывод краткой информации по нему без измерений, в URL добавляется его primary key
+class SensorView(RetrieveUpdateDestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+# обновление и удаление конкретного измерения, вывод информации по нему, в URL добавляется его primary key
+class MeasurementView(RetrieveUpdateDestroyAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+
+
