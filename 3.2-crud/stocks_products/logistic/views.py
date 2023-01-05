70a32cf474487b7308b08553b8a5303846b39c23
OlegPustovalov <106656451+OlegPustovalov@users.noreply.github.com>
2023-01-05 22:43:14 +0300

    Add views and serializers.py   ver 050123

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,4 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+#from django.db.models import Q
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -8,9 +13,13 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
-
-
+    #DEFAULT_FILTER_BACKENDS в setting.py
+    filterset_fields = ['title', 'description']
+    
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    #при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products',]
+
+    pagination_class = LimitOffsetPagination
